b21e4043903308f5065114af207982bdbbf1a7eb
Sasindu Avishka <sinharaavishka@gmail.com>
2021-03-21 19:42:32 +0530

    amgius commit 2

---
+++ app/components/GameData.js
@@ -0,0 +1,61 @@
+export default GameData = [
+    {
+        id:1,
+        title: "Unforseen Incident",
+        teaser: "A classical style interactive mystery set in a beautifully hand-painted world.",
+        description: "many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
+        rating: 4.5,
+        category: ["Action", "Indie"],
+        age:"12+",
+        cover: require("../assets/unforseen1.jpg"),
+        screenshots:[
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+        ],
+
+        backgroundColor: "#824571",
+    },
+
+    {
+        id:2,
+        title: "Unforseen Incident",
+        teaser: "A classical style interactive mystery set in a beautifully hand-painted world.",
+        description: "many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
+        rating: 4.5,
+        category: ["Strategy", "Indie","Action"],
+        age:"12+",
+        cover: require("../assets/unforseen1.jpg"),
+        screenshots:[
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+        ],
+
+        backgroundColor: "#824571",
+    },
+
+    {
+        id:3,
+        title: "Unforseen Incident",
+        teaser: "A classical style interactive mystery set in a beautifully hand-painted world.",
+        description: "many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
+        rating: 4.5,
+        category: ["Action", "Indie"],
+        age:"12+",
+        cover: require("../assets/unforseen1.jpg"),
+        screenshots:[
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+            require("../assets/unforseen1.jpg"),
+        ],
+
+        backgroundColor: "#824571",
+    },
+];
--- app/components/screens/HomeScreen.js
+++ app/components/screens/HomeScreen.js
@@ -1,16 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import { View, StyleSheet,StatusBar,Image } from 'react-native';
 import styled from "styled-components";
 import Text from "../Text";
 import CategoryList from "../Categories";
+import GameData from "../GameData";
+
 
 
 
 function HomeScreen(props) {
     const [selectedCategory, setSelectedCategory] = useState("All");
+    const gamesRef = useRef();
 
     const changeCategory = (category) =>{
+        gamesRef.current.scrollToOffset({x:0, y:0}); 
         setSelectedCategory(category);
+    }
+
+    const GameItem = (game) =>{
+
+        return (
+            <Game>
+                <GameCover source={game.cover} />
+
+                <GameInfo backgroundColor={game.backgroundColor}>
+                    <GameImage source={game.cover} />
+                    <GameTitle>
+                        <Text medium bold>{game.title}</Text>
+                        <Text small>{game.teaser}</Text>
+                    </GameTitle>
+                </GameInfo>
+            </Game>
+        )
     }
     return (
         <Container>
@@ -36,10 +57,19 @@ function HomeScreen(props) {
                             <CategoryName selected={selectedCategory === category ? true :false}>
                                 {category}
                             </CategoryName>
+                            {selectedCategory === category && <CategoryDot />}
                         </Category>
                     )
                 })}
             </Categories>
+            <Games data={GameData.filter(game => {
+
+                return game.category.includes(selectedCategory) || selectedCategory === "All";
+
+            })} keyExtractor={item => String(item.id)} renderItem={({item}) => GameItem(item)}
+                ref={gamesRef}
+            
+            />
 
         </Container>
 
@@ -85,6 +115,58 @@ const CategoryName = styled(Text)`
 
     color: ${(props) => (props.selected ? "#819ee5" : "#9a9a9a")};
     font-weight: ${(props) => (props.selected ? "700" : "500")};
+
+`;
+
+const Games = styled.FlatList`
+    margin-top: 32px;
+    flex: 1;
+
+`; 
+
+const CategoryDot = styled.View`
+
+    width: 6px;
+    height: 6px;
+    border-radius: 3px;
+    background-color: #819ee5;
+
+`;
+
+const Game = styled.TouchableOpacity`
+
+    margin-bottom: 32px;
+
+`;
+
+const GameCover = styled.Image`
+
+    height: 300px;
+    width: 100%;
+
+`; 
+
+const GameInfo = styled.View`
+
+    margin: -50px 16px 0 16px;
+    padding: 16px;
+    border-radius: 12px;
+    flex-direction: row;
+    align-items: center;
+
+`;
+
+const GameImage = styled.Image`
+
+    width: 50px;
+    height: 40px;
+    border-radius: 8px;
+
+`;
+
+const GameTitle = styled.View`
+
+    margin: 0 24px;
 
 `;
 
