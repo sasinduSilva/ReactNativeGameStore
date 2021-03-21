f02d518bea28cbe3459499f8d43a2411a9919dd4
Sasindu Avishka <sinharaavishka@gmail.com>
2021-03-21 20:10:03 +0530

    added navigation

--- App.js
+++ App.js
@@ -2,11 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from "./app/components/screens/HomeScreen";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createStackNavigator} from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+
+const AppStack = createStackNavigator();
+const TabNav = createBottomTabNavigator();
+
+const TabNavScreen = () => {
+  return(
+    <TabNav.Navigator>
+      <TabNav.Screen name="HomeScreen" component={HomeScreen} />
+    </TabNav.Navigator>
+  )
+}
 
 
 export default function App() {
   return (
-    <HomeScreen />
+    <NavigationContainer>
+      <AppStack.Navigator headerMode="none">
+          <AppStack.Screen name="App" component={TabNavScreen} />
+      </AppStack.Navigator>
+    </NavigationContainer>
   );
 }
 
