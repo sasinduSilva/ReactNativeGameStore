587a57b8497ea299263e8494b28e5e941263c2a6
Sasindu Avishka <sinharaavishka@gmail.com>
2021-03-21 16:04:46 +0530

    Category on select color change working

---
+++ app/components/Categories.js
@@ -0,0 +1,13 @@
+export default CategoryList = [
+    "All",
+    "Action",
+    "Adventure",
+    "Casual",
+    "Indie",
+    "Multiplayer",
+    "Racing",
+    "RPG",
+    "Simulation",
+    "Sports",
+    "Strategy",
+];
--- app/components/screens/HomeScreen.js
+++ app/components/screens/HomeScreen.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { View, StyleSheet,StatusBar } from 'react-native';
+import React, {useState} from 'react';
+import { View, StyleSheet,StatusBar,Image } from 'react-native';
 import styled from "styled-components";
 import Text from "../Text";
+import CategoryList from "../Categories";
 
 
 
 function HomeScreen(props) {
+    const [selectedCategory, setSelectedCategory] = useState("All");
+
+    const changeCategory = (category) =>{
+        setSelectedCategory(category);
+    }
     return (
         <Container>
 
@@ -21,8 +27,19 @@ function HomeScreen(props) {
                         Best games for today
                     </Text>
                 </Text>
-                <Avatar source={require('')} />
+                <Avatar source={require("../../assets/EFTLogo.png")} />
             </Header>
+            <Categories horizontal={true} showHorizontalScrollIndicator={false}>
+                {CategoryList.map((category,index) => {
+                    return (
+                        <Category key={index} onPress={() => changeCategory(category)}>
+                            <CategoryName selected={selectedCategory === category ? true :false}>
+                                {category}
+                            </CategoryName>
+                        </Category>
+                    )
+                })}
+            </Categories>
 
         </Container>
 
@@ -36,8 +53,39 @@ const Container = styled.SafeAreaView`
     
     `;
 
-const Header = styled.View``;
+const Header = styled.View`
+    flex-direction:row;
+    align-items:center;
+    justify-content: space-between;
+    margin: 10px 32px 0 32px;
+`;
 
-const Avatar = styled.Image``;
+const Avatar = styled.Image`
+    width:40px;
+    height:40px;
+
+`;
+
+const Categories = styled.ScrollView`
+
+    margin-top: 32px;
+    flex-grow: 0;
+
+
+`;
+
+const Category = styled.TouchableOpacity`
+    align-items:center;
+    margin: 0 16px;
+    height: 32px;
+
+`;
+
+const CategoryName = styled(Text)`
+
+    color: ${(props) => (props.selected ? "#819ee5" : "#9a9a9a")};
+    font-weight: ${(props) => (props.selected ? "700" : "500")};
+
+`;
 
 export default HomeScreen;
